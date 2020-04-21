594c4b4d4aa82b06517d91ebb67171392a476a27
Craig N. Caroon <craig.caroon@cengage.com>
2020-04-21 16:37:29 -0400

    ~ DRY up UI Components

--- src/renderer/components/LogEntries.vue
+++ src/renderer/components/LogEntries.vue
@@ -1,44 +1,11 @@
 <template>
   <v-container>
-    <v-app-bar app dense fixed dark clipped-left>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Ĉartaro - Log Entries</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-row no-gutters align="center">
-        <v-col cols="1">
-          <v-toolbar-items>
-            <v-btn icon @click.stop="newEntry()">
-              <v-icon>mdi-file-document</v-icon>
-            </v-btn>
-          </v-toolbar-items>
-        </v-col>
-        <v-col cols="8">
-          <v-toolbar-items>
-            <v-pagination
-              v-model="page"
-              :length="Math.ceil(totalEntries/perPage)"
-              total-visible="10"
-              @input="load"
-            ></v-pagination>
-          </v-toolbar-items>
-        </v-col>
-        <v-col>
-          <v-toolbar-items>
-            <v-text-field
-              ref="searchBox"
-              v-model="searchText"
-              dense
-              clearable
-              placeholder="Search..."
-              prepend-inner-icon="mdi-magnify"
-              @click:clear="clearSearch"
-              @keyup.enter="search()"
-              @keyup.esc="clearSearch()"
-            ></v-text-field>
-          </v-toolbar-items>
-        </v-col>
-      </v-row>
-    </v-app-bar>
+    <AppBar
+      v-bind:name="'Log Entries'"
+      v-bind:numPages="Math.ceil(totalEntries/perPage)"
+      v-bind:newItem="newEntry"
+      v-bind:refresh="refresh"
+    ></AppBar>
     <LogEntryEditor v-model="showEditor" v-bind:logEntry="logEntry" v-on:close="closeEditor"></LogEntryEditor>
     <LogEntryViewer v-model="showViewer" v-bind:logEntry="logEntry" v-on:close="closeViewer"></LogEntryViewer>
     <v-list dense>
@@ -58,26 +25,10 @@
           </v-list-item-title>
           <v-list-item-subtitle>
             {{ logEntry.category }} | {{ logEntry.logged_at ? format.formatDate(logEntry.logged_at*1000) : '--'}}
-            <v-chip
-              x-small
-              label
-              class="mr-1 float-right"
-              :color="rowColor(idx+1)"
-              v-for="(tag,tgIdx) in logEntry.tags"
-              :key="tgIdx"
-            >{{ tag }}</v-chip>
+            <Tags v-bind:tags="logEntry.tags" v-bind:color="rowColor(idx+1)"></Tags>
           </v-list-item-subtitle>
         </v-list-item-content>
-        <v-list-item-action>
-          <v-btn icon outlined @click="edit(logEntry)">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-        </v-list-item-action>
-        <v-list-item-action>
-          <v-btn icon outlined @click="remove(logEntry)">
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </v-list-item-action>
+        <Actions v-bind:actions="{edit: edit, remove: remove}" v-bind:item="logEntry"></Actions>
       </v-list-item>
     </v-list>
   </v-container>
@@ -89,12 +40,15 @@ import Mousetrap from 'mousetrap'
 import Constants from '../lib/Constants'
 import Format from '../lib/Format'
 
+import Actions from './Shared/Actions'
+import AppBar from './Shared/AppBar'
 import LogEntryEditor from './LogEntries/Editor'
 import LogEntryViewer from './LogEntries/Viewer'
+import Tags from './Shared/Tags'
 
 export default {
   name: 'logEntries-main',
-  components: { LogEntryEditor, LogEntryViewer },
+  components: { Actions, AppBar, LogEntryEditor, LogEntryViewer, Tags },
   mounted: function () {
     this.bindShortcutKeys()
     this.load()
@@ -115,9 +69,21 @@ export default {
       })
     },
 
+    refresh: function (page = null, searchText = '') {
+      if (page !== null) {
+        this.page = page
+      }
+
+      if (searchText !== '') {
+        this.searchText = searchText
+      }
+
+      this.load()
+    },
+
     load: function () {
       var self = this
-      var qs = `page=${self.page}&pp=${self.perPage}`
+      var qs = `page=${this.page}&pp=${this.perPage}`
 
       if (this.searchText) {
         var parts = this.searchText.split(':', 2)
@@ -172,7 +138,7 @@ export default {
 
     closeEditor: function () {
       this.showEditor = false
-      this.load()
+      this.refresh()
     },
 
     closeViewer: function () {
@@ -186,23 +152,6 @@ export default {
         color = Constants.COLORS.GREY_ALT
       }
       return color
-    },
-
-    search: function () {
-      if (this.searchText) {
-        this.page = 1
-        this.load()
-      }
-    },
-
-    clearSearch: function () {
-      if (this.searchText) {
-        this.page = 1
-        this.searchText = null
-        this.load()
-      }
-
-      this.$refs.searchBox.blur()
     }
   },
 
---
+++ src/renderer/components/Shared/Tags.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>
+    <v-chip
+      x-small
+      label
+      class="mr-1 float-right"
+      :color="color"
+      v-for="(tag,tgIdx) in tags"
+      :key="tgIdx"
+    >{{ tag }}</v-chip>
+  </span>
+</template>
+<script>
+export default {
+  name: 'shared-tags',
+  components: {},
+  props: ['tags', 'color'],
+
+  methods: {},
+
+  data () {
+    return {}
+  }
+}
+</script>
